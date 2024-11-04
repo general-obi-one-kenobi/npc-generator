3d6fd8f93766e21fe81ed9fab68906690da9f837
general-obi-one-kenobi <aadi.aiyer@gmail.com>
2024-11-04 09:12:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import random
+
 # This is the intro
 print('Welcome to Alien NPC Generator')
 print('\n')
+
 # This is the variable setup
 name = ''
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
